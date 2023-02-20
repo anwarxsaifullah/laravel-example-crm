9dbe33981b7b005b18a5f07f251926cb63b54901
Anwar <anwarsaifullah727@gmail.com>
2023-02-20 09:07:24 +0700

    Add $fillable to Companies and Employees models

--- app/Models/Companies.php
+++ app/Models/Companies.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Companies extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'website',
+        'logo',
+    ];
+
 }
